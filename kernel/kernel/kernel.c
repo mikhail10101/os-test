5216f8b22570204f8ca26462960d65ef7240b57d
mikhail10101 <mikhail.borbe.lg@gmail.com>
2025-06-16 22:18:57 -0700

    Created itoa.c function that takes an integer and stores its interpretation as a string in a provided buffer

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <kernel/tty.h>
 
-void kernel_main(void) {
+void kernel_main(int rec) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+
+	char buff[200];
+	printf("%s", itoa(rec, buff));
 }
